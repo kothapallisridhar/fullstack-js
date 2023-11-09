1b937c74150cb4fcb837f20255b64a4fbc53b42c
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2023-11-09 16:08:27 +0530

    map function

--- higher-order-function/arrayfunctions.js
+++ higher-order-function/arrayfunctions.js
@@ -2,3 +2,16 @@
 // index of element is the key and the element itself is the value
 
 // Higher Order Functions: These are functions that depends & operate on other function. Higher Order Functions takes another function as an argument or return a function as an argument.
+
+/**
+ * map function
+ *
+ */
+const arr = [1, 2, 3, 4, 5];
+
+function square(el) {
+  return el * el;
+}
+
+const result = arr.map(square);
+console.log(result);
