928008f09ac48b18671ddfc253bae1f303cef77b
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2023-11-09 16:57:15 +0530

    function with 2 arguments

--- higher-order-function/arrayfunctions.js
+++ higher-order-function/arrayfunctions.js
@@ -17,3 +17,9 @@ const result = arr.map(square);
 console.log(result);
 
 /** If the function that we are passing in map takes two arguments then the first argument will be a accessing the actual value and the second argument will be accessing index of that value. */
+
+function fun2(el, ind) {
+  console.log(`Element at index ${ind} is ${el}`);
+}
+arr2 = [15, 32, 66, 64, 83, 89];
+arr2.map(fun2);
