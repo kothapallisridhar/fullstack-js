160fef24e553c3e0f1d9b78b86e59ca8d7a6af0d
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2023-11-09 16:02:44 +0530

    HoF

--- higher-order-function/arrayfunctions.js
+++ higher-order-function/arrayfunctions.js
@@ -1,2 +1,4 @@
 // arrays are custom objects in JS
 // index of element is the key and the element itself is the value
+
+// Higher Order Functions: These are functions that depends & operate on other function. Higher Order Functions takes another function as an argument or return a function as an argument.
