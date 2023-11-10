1676c1b7044654e13556fb92ac751d1698d0925c
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2023-11-10 08:50:30 +0530

    Inversion of Control

--- callbacks-promises/callbacks.js
+++ callbacks-promises/callbacks.js
@@ -1,8 +1,18 @@
-setTimeout(() => {
-  console.log("Hello JavaScript");
-}, 3000);
+// setTimeout(() => {
+//   console.log("Hello JavaScript");
+// }, 3000);
 /**
  *  Problems with Callbacks
  *  1. Inversion of Control
  *  2. Callback hell -> It's a readability problem
  */
+// Inversion of Control
+function doTask(fn, x) {
+  // implementation is done by team A
+  fn(x);
+}
+
+// due to callbacks, I'm passing control of how exec() should be called to doTask(). This is Inversion of Control
+doTask(function exec(x) {
+  console.log("Woah! number is: ", x * x);
+}, 9);
