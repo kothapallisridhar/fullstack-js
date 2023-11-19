4801e8ac5e2b2f26f953c9d406582901ab5f676a
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2023-11-19 21:47:47 +0530

    js is synchronous

---
+++ js-synchronous/synchronous.js
@@ -0,0 +1,7 @@
+/**
+ *  JavaScript is Synchronous in nature
+ *  JavaScript is Single Threaded
+ *  (if we execute valid ECMA script code viz is given by the standards)
+ *
+ *  viz => which is
+ */
