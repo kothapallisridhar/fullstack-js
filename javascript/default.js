8db15aeb6f90a3894787124f35f48d2c03c8fb4a
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2023-11-19 10:28:26 +0530

    coercion

---
+++ javascript/default.js
@@ -0,0 +1,13 @@
+console.log(1 - 10); // -9
+console.log(1 - "453"); // 1- 453 = -452
+console.log(1 - null); // 1 - 0 = 1
+console.log(1 - undefined); // 1 - NaN = NaN
+console.log(1 - "12tyr04"); // 1 - NaN = NaN
+console.log(1 - "0xa"); // 1 - 10 = -9
+
+console.log(1 + 10); // 11
+console.log(1 + "453"); // "1" + "453" = 1453
+console.log(1 + null); // 1 + 0 = 1
+console.log(1 + undefined); // 1 + NaN = NaN
+console.log(1 + "23tyR1"); // "1" - "23tyR1" = 123tyR1
+console.log(1 + "0xa"); // "1" + "0xa" = 10xa
