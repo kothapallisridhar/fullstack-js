9582409ce765704a3e89fc75909984ec656c05f6
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2023-11-20 15:47:25 +0530

    comment

--- js-sync-async/setinterval-example.js
+++ js-sync-async/setinterval-example.js
@@ -2,5 +2,5 @@ let x = setInterval(function () {
   console.log("Hello World!");
 }, 1000);
 
-console.log(typeof x);
+console.log(typeof x); // return type is "object" in node environment
 clearInterval(x);
