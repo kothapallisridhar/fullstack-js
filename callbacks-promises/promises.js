720303ace68f3899ab58b8ff96a0c706c6827cf0
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2023-11-20 16:12:48 +0530

    Promises

---
+++ callbacks-promises/promises.js
@@ -0,0 +1,17 @@
+/**
+ *  Readability Enhancers
+ *
+ *  Promises solve the problem of Inversion of Control (IoC)
+ *
+ *  In JavaScript, Promises are special type of Objects that get
+ *  returned immediately when we call them
+ *
+ *  Promises acts as a placeholder for the data we hope to get back
+ *  sometime in future
+ *
+ *  In these Promise objects, we can attach the functionality we want to execute
+ *  once the future task is done.
+ *
+ *  Once the future task is done, Promises will automatically execute the
+ *  attached functionality.
+ */
