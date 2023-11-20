9a16cfadfaea550bc88b6b44d08402883e58c3c3
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2023-11-20 12:48:29 +0530

    interview problem 2

---
+++ js-sync-async/imp-int-problem2.js
@@ -0,0 +1,9 @@
+// predict the output of following code
+console.log("Hello World!");
+setTimeout(function exec() {
+  console.log("Timer ends");
+}, 0);
+for (let i = 0; i < 10000000000; i++) {
+  // some task
+}
+console.log("End");
