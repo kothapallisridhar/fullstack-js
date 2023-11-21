47608239663a8cbccb70e29f4af9476cea4e978e
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2023-11-21 22:30:42 +0530

    comments added

--- callbacks-promises/callback-example.js
+++ callbacks-promises/callback-example.js
@@ -1,4 +1,13 @@
+// Don't use promises. Only use callbacks
+// 1. Write a function to download data from a url
+// 2. Write a function to save that downloaded data in a file and return the file name
+// 3. Write a function to upload the file written in previous step to a new url.
+
 function download(url, cb) {
+  /**
+   * Downloads content from the given url and passed the
+   * downloaded content to the given callback cb
+   */
   console.log("Starting to download data from: ", url);
   setTimeout(function down() {
     console.log("Download completed");
