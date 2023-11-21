b04c118e96a257bf70e92ded0f59bf0351ca3b9d
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2023-11-21 13:30:41 +0530

    eventloop and microtask queue

--- callbacks-promises/eventloop-microtask.js
+++ callbacks-promises/eventloop-microtask.js
@@ -1,13 +1,12 @@
 function createPromise() {
   return new Promise(function exec(resolve, reject) {
-    console.log("Resolving the Promise");
-    resolve("Promise RESOLVED");
+    setTimeout(function process() {
+      console.log("Timer completed");
+      console.log("Rejecting the Promise");
+      reject("Promise REJECTED");
+    }, 0);
   });
 }
-
-setTimeout(function process() {
-  console.log("Timer completed");
-}, 0);
 
 let p = createPromise();
 
@@ -15,19 +14,17 @@ p.then(
   function fulfillHandler1(value) {
     console.log("Fulfill handler 1 completed with value: ", value);
   },
-  function rejectHandler1() {}
+  function rejectHandler1(value) {
+    console.log("Reject handler 1 completed with value: ", value);
+  }
 );
 p.then(
   function fulfillHandler2(value) {
     console.log("Fulfill handler 2 completed with value: ", value);
   },
-  function rejectHandler1() {}
-);
-p.then(
-  function fulfillHandler3(value) {
-    console.log("Fulfill handler 3 completed with value: ", value);
-  },
-  function rejectHandler1() {}
+  function rejectHandler1(value) {
+    console.log("Reject handler 2 completed with value: ", value);
+  }
 );
 
 for (let i = 1; i < 10000000000; i++) {
