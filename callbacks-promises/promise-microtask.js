067318c69e4030dd44387749041e9733f5dd20a4
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2023-11-21 13:20:00 +0530

    Microtask Queue

---
+++ callbacks-promises/promise-microtask.js
@@ -0,0 +1,36 @@
+function createPromise() {
+  return new Promise(function exec(resolve, reject) {
+    console.log("Resolving the Promise");
+    resolve("Promise RESOLVED");
+  });
+}
+
+setTimeout(function process() {
+  console.log("Timer completed");
+}, 0);
+
+let p = createPromise();
+
+p.then(
+  function fulfillHandler1(value) {
+    console.log("Fulfill handler 1 completed with value: ", value);
+  },
+  function rejectHandler1() {}
+);
+p.then(
+  function fulfillHandler2(value) {
+    console.log("Fulfill handler 1 completed with value: ", value);
+  },
+  function rejectHandler1() {}
+);
+p.then(
+  function fulfillHandler3(value) {
+    console.log("Fulfill handler 1 completed with value: ", value);
+  },
+  function rejectHandler1() {}
+);
+
+for (let i = 1; i < 10000000000; i++) {
+  //some task
+}
+console.log("Ending");
