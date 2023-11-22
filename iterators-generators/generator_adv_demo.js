4a0bc9373c0b73d3ee9d2db257be1da5515de8ec
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2023-11-22 18:22:28 +0530

    adv generators

--- iterators-generators/generator_adv_demo.js
+++ iterators-generators/generator_adv_demo.js
@@ -5,6 +5,7 @@ function* fetchNextElement() {
   console.log("entering after a yield");
   const y = x + (yield 30);
   console.log("value of y is:", y);
+  yield 29;
 }
 
 console.log("start");
@@ -13,3 +14,4 @@ console.log("called generator");
 console.log(iter.next());
 console.log(iter.next());
 console.log(iter.next(17));
+console.log(iter.next());
