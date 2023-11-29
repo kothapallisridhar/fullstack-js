af8bd592229ae11279f41f51d7b1aa635e1218af
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2023-11-29 10:03:40 +0530

    new keyword demo

---
+++ objects-classes-prototypes/new_keyword_demo.js
@@ -0,0 +1,16 @@
+class Product {
+  name;
+  price;
+  description;
+  constructor(name, price, description) {
+    this.name = name;
+    this.price = price;
+    this.description = description;
+    // return 10; // primitive => no effect
+    // return {x:10}
+    // return this;  // if you don't return anything, it is equal to saying return this
+  }
+  display() {}
+}
+const p = new Product("bag", 100, "a cool bag");
+console.log(p);
