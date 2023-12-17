47d241f4991591bc54ed0c3992df1ff2cd60048b
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2023-12-17 14:45:28 +0530

    type object

---
+++ typescript/start-up.js
@@ -0,0 +1,12 @@
+var age = 26;
+var isLight = true;
+var greatMan = "Sridhar";
+var user;
+user = {
+    name: "Sri",
+    age: "36",
+    id: "abc",
+    isAdmin: true,
+};
+user = {};
+console.log(user);
--- typescript/start-up.ts
+++ typescript/start-up.ts
@@ -1,3 +1,14 @@
-let a: number = 26;
+let age: number = 26;
 let isLight: boolean = true;
 let greatMan: string = "Sridhar";
+
+let user: object;
+
+user = {
+  name: "Sri",
+  age: "36",
+  id: "abc",
+  isAdmin: true,
+};
+user = {};
+console.log(user);
