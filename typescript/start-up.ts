a325e8b095953abeff0b00ed633a4a6dc6272f8d
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2023-12-18 08:12:30 +0530

    defining function types

--- typescript/start-up.ts
+++ typescript/start-up.ts
@@ -17,3 +17,14 @@ let hobbies: Array<string>;
 hobbies = ["sports", "reading", "cooking"];
 
 console.log(hobbies);
+
+function add(a: number, b: number): number {
+  return a + b;
+}
+function calculate(
+  a: number,
+  b: number,
+  calcFn: (a: number, b: number) => number
+) {
+  calcFn(a, b);
+}
