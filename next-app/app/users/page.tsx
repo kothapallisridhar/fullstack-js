62631211e340a2c308df1f9e45521a71518c0bdb
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2024-01-04 19:06:12 +0530

    using fetch

--- next-app/app/users/page.tsx
+++ next-app/app/users/page.tsx
@@ -1,11 +1,24 @@
 import Link from "next/link";
 import React from "react";
 
-const UsersPage = () => {
+interface User {
+  id: number;
+  name: string;
+}
+
+const UsersPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: User[] = await res.json();
   return (
     <>
       <div>UsersPage</div>
       <Link href="/users/new">New User</Link>
+      <h2>[ USERS ]</h2>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </>
   );
 };
