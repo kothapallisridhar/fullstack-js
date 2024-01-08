6c3776b7ccfa8e97e32952fb6ba8aabde275b6ef
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2024-01-08 18:50:08 +0530

    path module

--- first-app/app.js
+++ first-app/app.js
@@ -1,4 +1,8 @@
-var logger = require("./logger");
+const logger = require("./logger");
+const path = require("path");
+
+var pathObj = path.parse(__filename);
+console.log(pathObj);
 
 function sayHello(name) {
   console.log(`Hello ${name}`);
