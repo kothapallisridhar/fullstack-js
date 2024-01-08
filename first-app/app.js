a7b25e55801c1b2af0c05c84ecd97e7ec1d2109e
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2024-01-08 20:33:33 +0530

    fs module

--- first-app/app.js
+++ first-app/app.js
@@ -1,6 +1,7 @@
 const logger = require("./logger");
 const path = require("path");
 const os = require("os");
+const fs = require("fs");
 
 var pathObj = path.parse(__filename);
 console.log(pathObj);
@@ -17,3 +18,8 @@ var freeMemory = os.freemem();
 
 console.log(`Total Memory ${totalMemory}`);
 console.log(`Free Memory ${freeMemory}`);
+var files = fs.readdir("./", (err, files) => {
+  if (err) console.log(err);
+  else console.log(`Result ${files}`);
+});
+console.log(files);
