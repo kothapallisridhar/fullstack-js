36b5b75397025a5f9168578a9348fb64c635edda
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2024-01-08 09:52:58 +0530

    ExpandableText component

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,5 +1,6 @@
 import Alert from "./components/Alert";
 import Cart from "./components/Cart";
+import ExpandableText from "./components/ExpandableText";
 import Like from "./components/Like";
 import ListGroup from "./components/ListGroup";
 import NavBar from "./components/NavBar";
@@ -47,6 +48,20 @@ function App() {
       <Like onClick={() => console.log("Clicked!")} />
       <NavBar cartItemsCount={cartItems.length} />
       <Cart cartItems={cartItems} onClear={() => setCartItems([])} />
+      <ExpandableText maxChars={30}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat
+        necessitatibus veniam perferendis soluta libero enim nostrum nam? Quis,
+        temporibus labore, libero error fugit itaque deleniti inventore
+        exercitationem vitae tempore at quas dolores voluptatum totam voluptatem
+        laudantium, excepturi incidunt nam dignissimos magnam! Dolor modi ab
+        animi, accusantium quia soluta repudiandae libero consequatur atque
+        autem fuga, eos aspernatur possimus? Rem debitis, autem tenetur veniam
+        reiciendis repudiandae voluptate mollitia? Dicta, laudantium. Itaque at
+        voluptatem quod fugit laudantium, architecto vel ex quam eius alias ab
+        quis voluptatum aspernatur adipisci neque unde ea! Tempore doloremque
+        eligendi libero nihil pariatur aliquam doloribus. Nesciunt ab
+        accusantium nihil?
+      </ExpandableText>
     </div>
   );
 }
