62c722e7a3f455981b150e4a715a9cc16bad904a
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2024-02-21 12:57:03 +0530

    Counter App

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,15 +1,33 @@
+import { useState } from "react";
 import "./App.css";
 
 export function App(props) {
+  let [count, setCount] = useState(0);
   return (
     <div className="Wrapper">
-      <h2>Background color changer</h2>
-      <input
-        type="color"
-        onChange={(e) => {
-          document.body.style.background = e.target.value;
+      <button
+        className="minus"
+        disabled={count === 0}
+        onClick={() => {
+          if (count > 0) {
+            setCount(count - 1);
+          }
         }}
-      />
+      >
+        -
+      </button>
+      <p>{count}</p>
+      <button
+        className="plus"
+        disabled={count === 10}
+        onClick={() => {
+          if (count < 10) {
+            setCount(count + 1);
+          }
+        }}
+      >
+        +
+      </button>
     </div>
   );
 }
