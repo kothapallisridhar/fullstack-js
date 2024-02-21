683e4062ec11778c6085f552bff8e9af6f8f9f55
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2024-02-21 12:35:15 +0530

    background color changer

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,24 +1,15 @@
-import { useState } from "react";
 import "./App.css";
 
 export function App(props) {
-  let [prodName, setProdName] = useState("Samsung");
   return (
-    <>
-      <h2>Product name: {prodName}</h2>
-      <h2>Product price: {156000}</h2>
-
-      <input id="pname" />
-      <button
-        onClick={() => {
-          let pname = document.getElementById("pname").value;
-          setProdName((prev) => {
-            return prev + pname;
-          });
+    <div className="Wrapper">
+      <h2>Background color changer</h2>
+      <input
+        type="color"
+        onChange={(e) => {
+          document.body.style.background = e.target.value;
         }}
-      >
-        Update Name
-      </button>
-    </>
+      />
+    </div>
   );
 }
