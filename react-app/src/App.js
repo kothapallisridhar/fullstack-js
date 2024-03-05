26e1ed545bd0837b56ba52eb059dfbcb8a254af8
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2024-03-05 10:33:23 +0530

    useMemo hook

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,23 +1,29 @@
 import "./App.css";
-import { createContext } from "react";
+import { createContext, useMemo, useState } from "react";
 import Home from "./Home";
 import Product from "./Product";
 
 export const context = createContext();
 
 export function App(props) {
-  let isAdmin = "true";
-  let p = {
-    name: "Canon EOS",
-    price: 333300,
-  };
+  const [counter, setCounter] = useState(1);
+  let num1 = 25;
+  let num2 = 70;
+  let sum = useMemo(() => {
+    return num1 + num2;
+  }, [num1, num2]);
 
   return (
     <>
-      <context.Provider value={p}>
-        <Home></Home>
-        <Product></Product>
-      </context.Provider>
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        Count
+      </button>
+      <h1>Counter: {counter}</h1>
+      <h2>Sum: {sum}</h2>
     </>
   );
 }
