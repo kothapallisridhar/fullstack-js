9752c9443e6cc26da080655d6570bc4700db45a1
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2024-03-17 13:03:40 +0530

    using Axios

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,75 +1,24 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  //const pwd = useRef();
-
-  const [error, setError] = useState({
-    email: "",
-    password: "",
+  let api = axios.create({
+    baseURL: "https://fakestoreapi.com",
   });
-  const emailpattern =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  useEffect(() => {
+    getProducts();
+  }, []);
 
-  function handleSubmit() {
-    if (email.trim() === "") {
-      setError((error) => ({ ...error, email: "Enter email address" }));
-    } else if (!emailpattern.test(email)) {
-      setError((error) => ({ ...error, email: "Enter valid email address" }));
-    } else {
-      setError((error) => ({ ...error, email: "" }));
-    }
-
-    if (password.trim() === "") {
-      setError((error) => ({ ...error, password: "Enter password" }));
-    } else if (password.trim().length < 8) {
-      setError((error) => ({
-        ...error,
-        password: "Password is minimum 8 characters",
-      }));
-    } else {
-      setError((error) => ({ ...error, password: "" }));
-    }
+  async function getProducts() {
+    const res = await api.get("/products");
+    console.log(res.data);
   }
-
   return (
     <>
-      <div className="border w-25 mt-5 m-auto p-3">
-        <h2 className="text-primary text-center">Login Form</h2>
-        <div className="mt-3">
-          <label>Email</label>
-          <input
-            type="email"
-            className="form-control"
-            value={email}
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
-          />
-          {error.email && <span className="text-danger">{error.email}</span>}
-        </div>
-        <div className="mt-3">
-          <label>Password</label>
-          <input
-            type="password"
-            className="form-control"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          {error.password && (
-            <span className="text-danger">{error.password}</span>
-          )}
-        </div>
-        <div className="mt-3">
-          <button className="btn btn-primary w-100" onClick={handleSubmit}>
-            Login
-          </button>
-        </div>
-      </div>
+      <h1>Using Axios</h1>
     </>
   );
 }
