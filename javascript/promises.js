b951143a7f6f298ef91c7d2bdc481f743ab1cc55
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2024-07-02 10:14:01 +0530

    comment

--- javascript/promises.js
+++ javascript/promises.js
@@ -1,6 +1,6 @@
 const GITHUB_API = "https://api.github.com/users/akshaymarch7";
 
-const user = fetch(GITHUB_API);
+const user = fetch(GITHUB_API); // fetch() returns a promise
 
 console.log();
 console.log(user);
