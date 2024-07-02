b473fff956e10e459ad3a1ca6897df9a7ab15609
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2024-07-02 10:20:21 +0530

    what is a promise?

--- javascript/promises.js
+++ javascript/promises.js
@@ -5,6 +5,10 @@ const user = fetch(GITHUB_API); // fetch() returns a promise
 // promise states: pending, fulfilled, rejected
 // data returned by promise is immutable
 
+// what is promise?
+// a container for a future value
+// A promise is an object representing eventual completion of an asynchronous operation.
+
 console.log();
 console.log(user);
 
