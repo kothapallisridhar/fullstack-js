337d6a8894ba696facc2895b8cadff1887eedbb1
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2024-07-02 10:17:45 +0530

    promise then

--- javascript/promises.js
+++ javascript/promises.js
@@ -2,5 +2,12 @@ const GITHUB_API = "https://api.github.com/users/akshaymarch7";
 
 const user = fetch(GITHUB_API); // fetch() returns a promise
 
+// promise states: pending, fulfilled, rejected
+// data returned by promise is immutable
+
 console.log();
 console.log(user);
+
+user.then(function (data) {
+  console.log(data);
+});
