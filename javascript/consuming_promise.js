ef7f3e0c3618e83d455a29f98b43bb1ae7d7b074
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2024-09-07 17:42:50 -0700

    Consuming Promise

--- javascript/consuming_promise.js
+++ javascript/consuming_promise.js
@@ -15,8 +15,17 @@ function createPromiseWithTimeout() {
         
     });
 }
+console.log("Starting");
 let p = createPromiseWithTimeout();
+console.log("We are now waiting for Promise to complete");
+console.log("Curretly my promise object is like....", p);
 
 p.
-then(function fulfillHandler(value) { console.log("Inside fulfill handler with value:", value)}, 
-     function rejectionHandler(value) { console.log("Inside rejection hanlder with value:", value)});
+then(function fulfillHandler(value) { 
+    console.log("Inside fulfill handler with value:", value);
+    console.log("Promise after fulfillment is: ", p)
+    }, 
+     function rejectionHandler(value) { 
+        console.log("Inside rejection hanlder with value:", value);
+        console.log("Promise after rejection is: ", p);
+    });
