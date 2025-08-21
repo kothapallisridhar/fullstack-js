11c5956cea2ecae04660fd6515f334a557246e2c
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-08-21 10:08:14 +0530

    types of functions

--- javascript/functions.js
+++ javascript/functions.js
@@ -6,10 +6,37 @@ function a() {
 //a();
 
 // Function Expression
-b();
+//b();
 var b = function() {
     console.log("b called")
 }
 //b();
 
-// Function Declaration
+// Function Anonymous Function
+// => function without a name is called anonymous function
+// can be used as values => assign it to a variable
+var c = function() {
+
+}
+
+// Named Function Expression
+var fe = function xyz() {
+    console.log("fe called")
+    console.log(xyz)
+}
+
+fe();
+//xyz(); // cannot call named function
+
+// Difference between Parameters & Arguments
+var ap = function(param1, param2) {
+    console.log(param1, param2)
+}
+ap(1,2) // 1,2 are arguments
+
+
+// First Class Functions
+
+
+
+// Arrow Functions
