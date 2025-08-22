4fddbfd6e230430c9e274bfe550cc7ac12131bee
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-08-22 19:42:01 +0530

    create promise

---
+++ javascript/promise-create.js
@@ -0,0 +1,31 @@
+const cart = ["shoes", "shorts", "shirts"];
+
+const promise = createOrder(cart); // orderId
+
+promise.then(function(orderId) {
+
+    console.log("OrderId: ", orderId)
+    //proceedToPayment(orderId);
+})
+
+function createOrder(cart) {
+    const pr = new Promise(function(resolve, reject) {
+        if(!validateCart(cart)) {
+            const err = new Error("Cart is not valid!");
+            reject(err);
+        } 
+        const orderId = "12345";
+        if(orderId) {
+            setTimeout(function() {
+                resolve(orderId);
+            },3000)
+            
+        }
+    });
+
+    return pr;
+}
+
+function validateCart(cart) {
+    return true;
+}
