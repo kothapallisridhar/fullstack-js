b19ff7c01daa5d11951b6f2577045e9178612466
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-08-23 12:28:09 +0530

    promise apis

--- javascript/promise-apis.js
+++ javascript/promise-apis.js
@@ -47,3 +47,55 @@
  * it will be an array of the error values of all of them
  * [err1, err2, err3]
  */
+
+const p1 = new Promise((resolve, reject) => {
+
+    setTimeout(() => {
+        resolve("P1 success");
+    }, 3000);
+
+})
+
+const p2 = new Promise((resolve, reject) => {
+
+    setTimeout(() => {
+        //resolve("P2 success")
+        reject("P2 fail");
+    }, 1000);
+
+})
+
+const p3 = new Promise((resolve, reject) => {
+
+    setTimeout(() => {
+        //resolve("P3 success");
+        reject("P3 fail");
+    }, 2000);
+
+})
+
+// Promise.all([p1, p2, p3]).then(res => {
+//     console.log(res);
+// }).catch(err => {
+//     console.error(err);
+// });
+
+// safest option among all other apis
+Promise.allSettled([p1, p2, p3]).then(res => {
+    console.log(res);
+}).catch(err => {
+    console.error(err);
+});
+
+// Promise.race([p1, p2, p3]).then(res => {
+//     console.log(res);
+// }).catch(err => {
+//     console.error(err);
+// });
+
+Promise.any([p1, p2, p3]).then(res => {
+    console.log(res);
+}).catch(err => {
+    console.error(err);
+});
+
