7d50c7055c9075bcb1866fdd54f171426782a3b6
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-08-23 22:10:09 +0530

    function currying

---
+++ javascript-interview/function-curry.js
@@ -0,0 +1,20 @@
+
+// let multiply = function(x, y) {
+//     console.log(x * y);
+// }
+
+let multiply = function(x) {
+    return function(y) {
+        console.log(x * y);
+    }
+}
+
+// using bind method
+// let multiplyByTwo = multiply.bind(this, 2);
+// multiplyByTwo(5);
+
+// let multiplyByThree = multiply.bind(this, 3);
+// multiplyByThree(5);
+
+let multiplyByTwo = multiply(2);
+multiplyByTwo(50);
