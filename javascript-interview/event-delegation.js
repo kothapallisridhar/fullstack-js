1c8688896821626058fcb2c858d2c9ed5694caa7
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-08-24 10:15:41 +0530

    event delegation

--- javascript-interview/event-delegation.js
+++ javascript-interview/event-delegation.js
@@ -9,3 +9,10 @@ document.querySelector("#category")
         window.location.href = "/" + e.target.id
     }
 })
+
+document.querySelector("#form").addEventListener('keyup', (e) => {
+    console.log(e);
+    if(e.target.dataset.uppercase != undefined) {
+        e.target.value = e.target.value.toUpperCase();
+    }
+})
