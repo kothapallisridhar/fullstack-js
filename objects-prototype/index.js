d77cc68e8c310d98de4f9a61d8a2fdadad87e2a1
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-09-05 23:32:41 +0530

    hasOwnProperty and  in

--- objects-prototype/index.js
+++ objects-prototype/index.js
@@ -18,5 +18,20 @@ console.log(Object.getPrototypeOf(dog));
 
 console.log(Object.getPrototypeOf(animal));
 
+let student = {
+    name: "Sridhar",
+    phone: 8877788777,
+    address: "Munich, Germany",
+    marks: 99,
+}
+
+console.log(student.hasOwnProperty("marks"));
+
+const student1 = Object.create(student);
+console.log(student1);
+console.log(student1.hasOwnProperty("marks")); // "marks" property is in the parent.
+console.log(student1.marks);
+
+console.log("marks" in student1); // checks the "marks" in the entire prototype chain
 
 
