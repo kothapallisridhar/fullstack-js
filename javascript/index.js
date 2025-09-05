d0125f228c16d17116200b8cbf95de9126a85bc4
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-09-05 09:34:33 +0530

    logs

---
+++ javascript/index.js
@@ -0,0 +1,10 @@
+console.log("I'm Sridhar");
+console.log('I have bachelor\'s degree');
+console.log('I am the "best"');
+console.log("I am the", '"best"');
+console.log("I am\nlearning\nJavsScript\nfrom\nYouTube");
+console.log(`I am
+learning
+JavaScript
+from a 
+YouTube channel`);
