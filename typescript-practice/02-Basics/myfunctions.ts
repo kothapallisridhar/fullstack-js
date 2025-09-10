07f0b9c3b4a4d3c89607e8e7ecb7b6ea5f2cd9d1
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-09-10 23:02:12 +0530

    never return type

--- typescript-practice/02-Basics/myfunctions.ts
+++ typescript-practice/02-Basics/myfunctions.ts
@@ -21,4 +21,30 @@ loginUser("S", "s@s.com")
 addTwo(5)
 console.log(getUpper("Hello Sridhar"))
 
+// function getValue(myVal: number) {
+//     if(myVal > 5) {
+//         return true
+//     }
+//     return "200 OK"
+// }
+
+const getHello = (s: string): string => {
+    return ""
+}
+
+const heros = ['thor', 'spiderman', 'ironman']
+//const heros = [1, 2, 3]
+
+heros.map((hero): string => {
+    return `hero is ${hero}`
+})
+
+function consoleError(errmsg: string): void {
+    console.log(errmsg)
+}
+
+function handleError(errmsg: string): never {
+    throw new Error(errmsg)
+}
+
 export {}
