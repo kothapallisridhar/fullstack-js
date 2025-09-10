50da5ecece492f932cdc86c6ed5d6cfb3fff72df
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-09-10 08:42:32 +0530

    interfaces

--- typescript/index.js
+++ typescript/index.js
@@ -3,3 +3,61 @@ var firstName = "SaiSarath";
 var json = JSON.parse("55");
 console.log(typeof json);
 console.log(firstName);
+var userName = "Deiter";
+userName = 3;
+var otherName;
+var secondName;
+otherName = 'Klaas';
+//let sec = secondName as String;
+var sec = secondName;
+sec = "Jane";
+sec = "Stephane";
+//otherName = 2;
+//otherName = true; 
+var x;
+var w;
+var y = undefined;
+var z = y;
+z = 3;
+var n = null;
+// arrays
+var names = ["Klaas", "Lene", "Jane", "Kene"];
+var morenames = ["Kothapalli", "Sridhar"];
+//morenames.push("Sridhar");
+// tuple
+// datatypes to be defined, length is fixed
+var ourTuple;
+ourTuple = [3, true, "Codepro"];
+console.log(ourTuple);
+var num = ourTuple[0], bool = ourTuple[1], str = ourTuple[2];
+console.log("destructured values: ", num, bool, str);
+console.log(ourTuple);
+//ourTuple.push("something"); // works without readonly
+console.log(ourTuple);
+console.log(names[0]);
+// object type
+// let car:{make:string, model:string, year:number} = {
+//     make: 'Lamborghini',
+//     model: 'Urus',
+//     year: 2025,
+// }
+var car = {
+    make: 'Ferrari',
+};
+car.mileage = 7;
+var nameAgeMap = {};
+nameAgeMap.Jack = 25;
+// enum
+var CardinalDirections;
+(function (CardinalDirections) {
+    CardinalDirections[CardinalDirections["North"] = 1] = "North";
+    CardinalDirections[CardinalDirections["East"] = 2] = "East";
+    CardinalDirections[CardinalDirections["South"] = 3] = "South";
+    CardinalDirections[CardinalDirections["West"] = 4] = "West";
+})(CardinalDirections || (CardinalDirections = {}));
+console.log('North', CardinalDirections.North);
+console.log('East', CardinalDirections.East);
+console.log('South', CardinalDirections.South);
+console.log('West', CardinalDirections.West);
+var currentDirection = CardinalDirections.North;
+console.log(currentDirection);
--- typescript/index.ts
+++ typescript/index.ts
@@ -11,6 +11,126 @@ let userName: any = "Deiter";
 userName = 3;
 
 let otherName: unknown;
-otherName = 'Kumar';
-otherName = 2;
-otherName = true; 
+let secondName: unknown;
+otherName = 'Klaas';
+//let sec = secondName as String;
+let sec = <string>secondName;
+sec = "Jane";
+
+sec = "Stephane";
+//otherName = 2;
+//otherName = true; 
+
+let x:never;
+
+let w:undefined;
+let y:undefined=undefined;
+let z = y as unknown as any;
+z = 3;
+
+let n:null = null;
+
+// arrays
+
+const names:string[] = ["Klaas", "Lene", "Jane", "Kene"];
+
+const morenames: readonly string[] = ["Kothapalli", "Sridhar"];
+//morenames.push("Sridhar");
+
+// tuple
+// datatypes to be defined, length is fixed
+let ourTuple: [numType:number, boolType:boolean, strType:string];
+ourTuple = [3, true, "Codepro"];
+console.log(ourTuple)
+
+const [num, bool, str] = ourTuple;
+console.log("destructured values: ", num, bool, str);
+
+console.log(ourTuple)
+
+//ourTuple.push("something"); // works without readonly
+console.log(ourTuple);
+
+console.log(names[0]);
+
+// object type
+
+// let car:{make:string, model:string, year:number} = {
+//     make: 'Lamborghini',
+//     model: 'Urus',
+//     year: 2025,
+// }
+
+const car: { make: string, mileage?: number } = {
+    make: 'Ferrari',
+}
+
+car.mileage = 7;
+
+const nameAgeMap: {[index:string]: number} = {}
+
+nameAgeMap.Jack = 25;
+
+// enum
+enum CardinalDirections {
+    North = 1,
+    East,
+    South,
+    West,
+}
+
+console.log('North', CardinalDirections.North)
+console.log('East', CardinalDirections.East)
+console.log('South', CardinalDirections.South)
+console.log('West', CardinalDirections.West)
+
+let currentDirection = CardinalDirections.North;
+console.log(currentDirection);
+
+enum StatusCodes {
+    NotFound = 404, // '404' , 'some string' also accepted
+    Success = 200,
+    Accepted = 202,
+    BadRequest = 400,
+}
+
+type Name = string; // type alias
+type Id = number;
+let sume: Name = 'Sunil'; 
+
+type CarYear = number;
+type CarModel = string;
+type CarMake = string;
+
+type Car = {
+    year: CarYear,
+    make: CarMake,
+    mode: CarModel,
+}
+
+const carYear: CarYear = 2025;
+const carMake: CarMake = 'Porsche';
+const carModel: CarModel = 'Panamera';
+
+const car2: Car = {
+    year: carYear,
+    make: carMake,
+    mode: carModel,
+};
+
+interface Rectangle {
+    height: number,
+    width: number,
+}
+
+interface ColoredRectangle extends Rectangle {
+    color: string,
+}
+
+const rectangle:Rectangle = {
+    height: 20,
+    width: 10,
+}
+
+
+
